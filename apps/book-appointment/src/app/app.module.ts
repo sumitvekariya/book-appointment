cb7d3ad8d7adbd709b753f621fa3e278c3250251
Sumit Vekariya <sumitvekariya7@gmail.com>
2022-06-10 11:20:38 +0530

    installed angular material

--- apps/book-appointment/src/app/app.module.ts
+++ apps/book-appointment/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
